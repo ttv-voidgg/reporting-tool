56c91c16428df2705e11561aa30d575610ca5091
ttv-voidgg <juancarlos.deborja@gmail.com>
2023-10-07 12:19:11 -0500

    Updates on Adding Reports
    reports saving as json
    add/remove reports
    - remove only when more than 1
    add/remove subdetails
    - remove only when more than 1
    add notes
    add pr link

--- src/TextBoxSample.jsx
+++ src/TextBoxSample.jsx
@@ -1,63 +1,149 @@
 import React, { useState } from 'react';
 
 const TextBoxSample = () => {
-  const [inputs, setInputs] = useState(['']); // State to store input values
+  const [inputs, setInputs] = useState([{ taskName: '', prLink: '', details: '', subDetails: [''], notes: '' }]);
 
-  const handleInputChange = (index, event) => {
+  const handleInputChange = (inputIndex, field, event) => {
     const values = [...inputs];
-    values[index] = event.target.value;
+    values[inputIndex][field] = event.target.value;
+    setInputs(values);
+  };
+
+  const handleAddSubDetail = (inputIndex) => {
+    const values = [...inputs];
+    values[inputIndex].subDetails.push('');
+    setInputs(values);
+  };
+
+  const handleRemoveSubDetail = (inputIndex, subIndex) => {
+    const values = [...inputs];
+    values[inputIndex].subDetails.splice(subIndex, 1);
     setInputs(values);
   };
 
   const handleAddInput = () => {
-    setInputs([...inputs, '']);
+    setInputs([...inputs, { taskName: '', prLink: '', details: '', subDetails: [''], notes: '' }]);
   };
 
   const handleRemoveInput = (index) => {
-    const values = [...inputs];
-    values.splice(index, 1);
-    setInputs(values);
+    if (inputs.length > 1) {
+      const values = [...inputs];
+      values.splice(index, 1);
+      setInputs(values);
+    }
+  };
+
+  const handleSave = () => {
+    const formattedData = inputs.map((input) => ({
+      'Task Name': input.taskName.trim(), // Remove leading/trailing spaces
+      'PR Link': input.prLink.trim(),
+      'Details': input.details.trim(),
+      'Sub details': input.subDetails.map((subDetail) => subDetail.trim()), // Trim each sub detail
+      'Notes': input.notes.trim(),
+    }));
+    console.log(JSON.stringify(formattedData, null, 2)); // Log the formatted data as JSON
   };
 
   return (
     <div className="">
-        <textarea
+      {inputs.map((input, inputIndex) => (
+        <div key={inputIndex}>
+          <textarea
             placeholder='Task Name'
             className="border rounded px-2 py-1 mr-2 mb-5 w-full"
             rows='2'
-        />    
-        <input
-            type="text"
+            value={input.taskName}
+            onChange={(event) => handleInputChange(inputIndex, 'taskName', event)}
+          />
+          <input
+            type='text'
             placeholder='PR Link (if applicable)'
             className="border rounded px-2 py-1 mr-2 mb-5 w-full"
-        />            
-      {inputs.map((input, index) => (
-        <div key={index} className="mb-4 flex">
-          <textarea
-            placeholder='Particulars'
-            className="border rounded px-2 py-1 mr-2 w-full"
-            value={input}
-            onChange={(event) => handleInputChange(index, event)}
+            value={input.prLink}
+            onChange={(event) => handleInputChange(inputIndex, 'prLink', event)}
           />
-          <button
-            className="bg-red-500 text-white rounded px-2 py-1"
-            onClick={() => handleRemoveInput(index)}
-          >
-            Remove
-          </button>
-        </div>
-      ))}
-        <textarea
+          <div className="mb-4 flex items-center">
+            <textarea
+              placeholder='Details'
+              className="border rounded px-2 py-1 mr-2 w-full flex-grow"
+              value={input.details}
+              onChange={(event) => handleInputChange(inputIndex, 'details', event)}
+            />
+            <button
+              className={`rounded h-8 w-8 ${
+                inputs.length === 1 ? 'disabled' : 'enabled'
+              }`}
+              onClick={() => handleRemoveInput(inputIndex)}
+              disabled={inputs.length === 1}
+            >
+              -
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded ml-2 h-8 w-8"
+              onClick={() => handleAddSubDetail(inputIndex)}
+            >
+              +
+            </button>
+          </div>
+          {input.subDetails.map((subDetail, subIndex) => (
+            <div key={subIndex} className="mb-4 ml-6 flex items-center">
+              <textarea
+                placeholder='Sub Details'
+                className="border rounded px-2 py-1 mr-2 w-full flex-grow"
+                value={subDetail}
+                onChange={(event) => {
+                  const values = [...inputs];
+                  values[inputIndex].subDetails[subIndex] = event.target.value;
+                  setInputs(values);
+                }}
+              />
+              <button
+                className={`rounded h-8 w-8 ${
+                  input.subDetails.length === 1 ? 'disabled' : 'enabled'
+                }`}
+                onClick={() => handleRemoveSubDetail(inputIndex, subIndex)}
+                disabled={input.subDetails.length === 1}
+              >
+                -
+              </button>
+            </div>
+          ))}
+          <textarea
             placeholder='Notes for this Task'
             className="border rounded px-2 py-1 mr-2 mb-5 w-full"
             rows='5'
-        />       
+            value={input.notes}
+            onChange={(event) => handleInputChange(inputIndex, 'notes', event)}
+          />
+        </div>
+      ))}
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleAddInput}
       >
         Add Report
       </button>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2"
+        onClick={handleSave}
+      >
+        Save
+      </button>
+      <style>
+        {`
+          .disabled {
+            opacity: 0.5;
+            background-color: #ccc;
+            cursor: not-allowed;
+          }
+
+          .enabled {
+            background-color: red;
+            color: white;
+            cursor: pointer;
+          }
+        `}
+      </style>
     </div>
   );
 };
