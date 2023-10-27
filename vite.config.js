d0457c743c03fc50b9dc48f342bc34b7157fafeb
ttv-voidgg <juancarlos.deborja@gmail.com>
2023-10-27 12:38:02 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Other Vite configurations...
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Example API server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api from the request path
+      },
+    },
+  },
+});
